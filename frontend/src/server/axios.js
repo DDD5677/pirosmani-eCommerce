cffd66ceb64630756d111516ffe7223418a2bd55
DDD5677 <djumaqulovdonisher@mail.ru>
2023-11-20 14:03:09 +0500

    change server to render.com

--- frontend/src/server/axios.js
+++ frontend/src/server/axios.js
@@ -2,7 +2,8 @@ import { getItem } from "@/helpers/localStorage";
 import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:3000/api/v1";
-axios.defaults.baseURL = "https://decco.space/api/v1";
+//axios.defaults.baseURL = "https://decco.space/api/v1";
+axios.defaults.baseURL = "https://pirosmani-backend.onrender.com/api/v1";
 axios.defaults.headers.common["Authorization"] = "Bearer " + getItem("token");
 
 export default axios;
