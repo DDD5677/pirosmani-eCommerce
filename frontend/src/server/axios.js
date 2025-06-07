6b730335b428fb26b760a53fe609444a708aa04c
Danisher <donisherdjumaqulov@gmail.com>
2025-06-07 22:22:59 +0500

    fixed bug

--- frontend/src/server/axios.js
+++ frontend/src/server/axios.js
@@ -2,7 +2,7 @@ import { getItem } from "@/helpers/localStorage";
 import axios from "axios";
 
 //axios.defaults.baseURL = "http://localhost:3000/api/v1";
-axios.defaults.baseURL = "https://pirosmani.danisher.uz/app/api/v1";
+axios.defaults.baseURL = "https://pirosmani.danisher.uz/api/v1";
 //axios.defaults.baseURL = "https://pirosmani-backend.onrender.com/api/v1";
 axios.defaults.headers.common["Authorization"] = "Bearer " + getItem("token");
 
